ea91a9ed7358368f0cce0a920cc47ab72e88c315
bladeofgods <bladeofgods@163.com>
2020-03-12 17:26:39 +0800

    add price slider widget

---
+++ lib/combine_demo/own_demo/price_slider/price_slier_widget.dart
@@ -0,0 +1,27 @@
+/*
+* Author : LiJiqqi
+* Date : 2020/3/12
+*/
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class PriceSliderWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+
+    return PriceSliderWidgetState();
+  }
+
+}
+
+class PriceSliderWidgetState extends State<PriceSliderWidget> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+}
